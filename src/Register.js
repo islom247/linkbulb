9631c2722afdf1b54d08e0581e8fa6b463e60bd4
islom247 <islomiddin.sodiqov@gmail.com>
2020-04-17 22:18:06 +0300

    small updates

--- src/Register.js
+++ src/Register.js
@@ -4,14 +4,31 @@ import "./styles.css";
 class Login extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    error: ""
   };
   handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
     });
+    if (e.target.checked) {
+      this.setState({
+        error: ""
+      });
+    }
   };
-  handleSubmit = e => {};
+  handleSubmit = e => {
+    e.preventDefault();
+    if (!document.getElementById("tos").checked) {
+      this.setState({
+        error: "You should agree to terms of services to register!"
+      });
+    } else {
+      this.setState({
+        error: ""
+      });
+    }
+  };
 
   render() {
     return (
@@ -48,10 +65,23 @@ class Login extends Component {
                   onChange={this.handleChange}
                 />
               </div>
+              <div className="left-align">
+                <label>
+                  <input type="checkbox" className="filled-in" />
+                  <span>
+                    <a id="tos" href="http://www.google.com">
+                      I agree to the terms of services
+                    </a>
+                  </span>
+                </label>
+              </div>
               <div className="input-field center-align">
                 <button className="btn blue darken-3 z-depth-3">
                   Register
                 </button>
+              </div>
+              <div>
+                <label id="error">{this.state.error}</label>
               </div>
             </form>
           </div>
